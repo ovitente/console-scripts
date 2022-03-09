faf5fd1407c4a7de3af393404c1a8cf49515290e
Ivan Kostrubin <ivan.kostrubin01@currys.co.uk>
2022-03-09 18:34:53 +0200

    autosave

--- snippets.sh
+++ snippets.sh
@@ -1,14 +1,16 @@
 COMMANDS_LIST='
-python3 -m http.server
 ag --nobreak --nonumbers --noheading . | fzf
 ansible all -m shell -a "echo test"
 cat .gitlab-ci.yml | pbcopy
+cat FILE | base64 -w0 | xclip -selection clipboard
+circleci config validate config.yaml
 docker build -t $(basename $(pwd)) .
 docker build -t $(basename $(pwd)) . --build-arg CI_PROJECT_NAME="$(basename $(pwd))" --build-arg CI_PROJECT_NAMESPACE="vcg"
-grep --line-buffered --color=never -r "" * | fzf
 git add . && git commit -a && git push
-circleci config validate config.yaml
-cat FILE | base64 -w0 | xclip -selection clipboard
+git config --global user.email "ivan.kostrubin01@currys.co.uk"
+git config --global user.name "Ivan Kostrubin"
+grep --line-buffered --color=never -r "" * | fzf
+python3 -m http.server
 '
 
 choise=$(echo -n "${COMMANDS_LIST}" | fzf )
