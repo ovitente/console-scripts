fa4c4df5b11a5eeebb29ad6cc02799cf710644a6
Ivan Kostrubin <ivan.kostrubin01@currys.co.uk>
2022-03-28 22:04:08 +0300

    autosave

--- snippets.sh
+++ snippets.sh
@@ -7,11 +7,12 @@ circleci config validate config.yaml
 docker build -t $(basename $(pwd)) .
 docker build -t $(basename $(pwd)) . --build-arg CI_PROJECT_NAME="$(basename $(pwd))" --build-arg CI_PROJECT_NAMESPACE="vcg"
 git add . && git commit -a && git push
+git commit --amend --author="Ivan Kostrubin <ivan.kostrubin01@currys.co.uk>"
 git config --global user.email "ivan.kostrubin01@currys.co.uk"
 git config --global user.name "Ivan Kostrubin"
-git commit --amend --author="Ivan Kostrubin <ivan.kostrubin01@currys.co.uk>"
 grep --line-buffered --color=never -r "" * | fzf
 python3 -m http.server
+rsync -av -P server:~/unsted/movie.mkv .
 '
 
 choise=$(echo -n "${COMMANDS_LIST}" | fzf )
