7f3338f016580b6ec5ea41476e20c0c6f1b6277c
Ivan Kostrubin <ivan.kostrubin01@currys.co.uk>
2023-02-04 17:31:40 +0200

    autosave

--- snippets.sh
+++ snippets.sh
@@ -7,8 +7,8 @@ circleci config validate config.yaml
 docker build -t $(basename $(pwd)) .
 docker build -t $(basename $(pwd)) . --build-arg CI_PROJECT_NAME="$(basename $(pwd))" --build-arg CI_PROJECT_NAMESPACE="vcg"
 git add . && git commit -a && git push
-git commit --amend --author="Ivan Kostrubin <ivan.kostrubin01@currys.co.uk>"
-git config --global user.email "ivan.kostrubin01@currys.co.uk"
+git commit --amend --author="Ivan Kostrubin <7spawn@gmail.com>"
+git config --global user.email "7spawn@gmail.com"
 git config --global user.name "Ivan Kostrubin"
 grep --line-buffered --color=never -r "" * | fzf
 grep -rnw './' -e 'testtosearch' # Search test in files
@@ -18,11 +18,11 @@ rsync -av -P server:~/unsted/movie.mkv .
 rsync -rvhH --delete --progress --exclude='Photos Library.photoslibrary' /Users/det/x7/Pictures /Volumes/
 shell openssl x509 -enddate -noout -in  %s
 ssh -C2qTnN -D 8080 proxy
-sudo lsof -i -P -n | grep LISTEN
-sudo lsof -i:22 ## see a specific port such as 22 ##
-sudo netstat -tulpn | grep LISTEN
-sudo nmap -sTU -O IP-address-Here
-sudo ss -tulpn | grep LISTEN
+sudo lsof -i -P -n | grep LISTEN # Port scan
+sudo lsof -i:22 # Specific port check
+sudo netstat -tulpn | grep LISTEN # Port scan
+sudo nmap -sTU -O IP-address-Here # Port scan # Port scan
+sudo ss -tulpn | grep LISTEN # Port scan
 '
 
 choise=$(echo -n "${COMMANDS_LIST}" | fzf )
