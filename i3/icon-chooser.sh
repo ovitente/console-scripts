5dc074cacc9b68fa99e45a6ca1deb9b6c9867192
Ivan Kostrubin <7spawn@gmail.com>
2023-05-24 17:01:15 +0300

    autosave

--- i3/icon-chooser.sh
+++ i3/icon-chooser.sh
@@ -1,37 +1,41 @@
-#!/USR/BIN/ENV BASH
+#!/usr/bin/env bash
 #
 # TODO: СДЕЛАТЬ НУМЕРОВАНЫЙ СПИСОК ИКОНОК И ПРИНИМАТЬ КАК АРГУМЕНТ ТОЛЬКО ИКОНКУ.
 #
 ICONS_LIST="\N \N  \N  \N \N \N \N \N \N \N \N \N \N \N \N \N \N \N \N \N"
 PRESET_LIST="
-CFG
-EDIT
-SSH
-X7
-TERMINAL
-JP
-I3
-CS"
+CFG |
+EDIT |
+SSH |
+X7 |
+TERMINAL |
+JP |
+I3 |
+CS | "
 
 NAMES_LIST="
+BROWSER
 ANSIBLE
 DEPLOY
 EDIT
 FIREFOX
 I3
-MSGR
+MESSENGER
 DEV
 STAGE
 PROD
 TERMINAL"
 
 # icon=$(echo -e "${ICONS_LIST}" | rofi -dmenu -p "SET ICON")
-prefix="$(echo -e "${PRESET_LIST}" | rofi -dmenu -p "SET PREFIX")"
-if [[ ! -z "$prefix" ]]; then
-  label="$icon $prefix | $(echo -e "${NAMES_LIST}" | rofi -dmenu -p "SET NAME")"
+CURRENT_NAME=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2 | cut -c 4-)
+answer="$(echo -e "${CURRENT_NAME}" | rofi -dmenu -p "CHANGE NAME")"
+if [[ ! -z "$answer" ]]; then
+# if [[ "$answer" -ne "$CURRENT_NAME" ]] || [[ ! -z "$answer" ]]; then
+  prefix="$(echo -e "${PRESET_LIST}" | rofi -dmenu -p "SET PREFIX")"
+  label="$prefix $(echo -e "${NAMES_LIST}" | rofi -dmenu -p "SET NAME")"
 else
-  label="$icon $prefix $(echo -e "${NAMES_LIST}" | rofi -dmenu -p "SET NAME")"
-
+  echo "$CURRENT_NAME"
+  exit 0
 fi
 
 echo "$label"
